8f65cad75f4a2b185f379c27b8f1b4c84a4aafe3
qiuxan <qiuxan@gmail.com>
2023-08-22 22:05:44 +1000

    Handling POST Requests

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
 const express = require('express');
+const { error } = require('console');
 
 const app = express();
-
+app.use(express.json());
 // app.get('/', (req, res) => {
 //     res.status(200).json({ message: '{hello from the server side}', app: "Natourss" });
 // });
@@ -22,7 +23,26 @@ app.get('/api/v1/tours', (req, res) => {
             tours
         }
     })
-})
+});
+
+app.post('/api/v1/tours', (req, res) => {
+    console.log(req.body);
+
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = { id: newId, ...req.body };
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+
+});
+
 const port = 3000;
 
 app.listen(port, () => {
