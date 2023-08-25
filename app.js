7de2ede026507322a71805e563dcef7b31a3ba93
qiuxan <qiuxan@gmail.com>
2023-08-25 20:31:03 +1000

    Serving Static Files

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const app = express();
 // 1) MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
-app.use((req, res, next) => {
-    console.log('Hello from the middleware');
-    next();
-})
+
+app.use(express.static(`${__dirname}/public`));
+// app.use((req, res, next) => {
+//     console.log('Hello from the middleware');
+//     next();
+// })
 
 //request time middleware
 app.use((req, res, next) => {
