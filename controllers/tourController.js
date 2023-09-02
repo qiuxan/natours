09f93466c3935634cd0a11fc20bb090ceea90443
qiuxan <qiuxan@gmail.com>
2023-09-02 21:04:21 +1000

    Aliasing -- middle ware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -182,3 +182,10 @@ exports.deleteTour = async (req, res) => {
     //     data: null
     // })
 }
+
+exports.aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+    next();
+} 
