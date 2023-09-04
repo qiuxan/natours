08ff520cbef682789e3eabbdc466dd67fe0a2736
qiuxan <qiuxan@gmail.com>
2023-09-04 22:06:42 +1000

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = mongoose.Schema({
     name: {
@@ -8,6 +9,7 @@ const tourSchema = mongoose.Schema({
         unique: true,
         trim: true,
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration'],
@@ -61,6 +63,17 @@ const tourSchema = mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function () {
     return this.duration / 7;
 })
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+});
+
+// tourSchema.post('save', function (doc, next) {
+//     console.log(doc);
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
