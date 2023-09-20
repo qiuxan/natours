17182b9f44d8fc1bc0d629dfd52218515345348d
qiuxan <qiuxan@gmail.com>
2023-09-20 12:19:11 +1000

    Geospatial Aggregation_ Calculating Distances

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,7 @@
 // import tourController from '../controllers/tourController.js';
 
 const express = require('express');
-const { getAllTours, creatTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan, getToursWithin } = require("../controllers/tourController");
+const { getAllTours, creatTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan, getToursWithin, getDistances } = require("../controllers/tourController");
 
 //require reviewController
 // const reviewController = require('../controllers/reviewController');
@@ -30,6 +30,10 @@ router.route('/monthly-plan/:year')
 
 router.route('/tours-within/:distance/center/:latlng/unit/:unit').get(getToursWithin);
 // /tours-within/233/center/34.111745,-118.113491/unit/mi
+
+//distances?distance=233&center=-40,45&unit=mi
+router.route('/distances/:latlng/unit/:unit').get(getDistances);
+
 
 router.route('/')
     .get(getAllTours)
