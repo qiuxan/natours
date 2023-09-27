d63ef0fcb7359f62aa554e8c9ed76fb7142f8a7c
qiuxan <qiuxan@gmail.com>
2023-09-27 20:05:23 +1000

    Logging out Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const { deleteMe, getAllUsers, createUser, getUser, updateUser, deleteUser, updateMe, getMe } = require("../controllers/userController");
 // require authController.js
-const { signup, login, forgotPassword, resetPassword, protect, updatePassword, restrictTo } = require("../controllers/authController");
+const { signup, login, forgotPassword, resetPassword, protect, updatePassword, restrictTo, logout } = require("../controllers/authController");
 
 const router = express.Router();
 
 // signup route
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/logout', logout);
 router.post('/forgetPassword', forgotPassword);
 router.patch('/resetPassword/:token', resetPassword);
 
