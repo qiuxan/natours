dcbf5bca2e549110372fda6c00df75fde62fc70f
qiuxan <qiuxan@gmail.com>
2023-09-30 21:29:44 +1000

    Updating User Data with Our API

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,15 @@
 import '@babel/polyfill';
 import { displayMap } from "./mapbox";
 import { login, logout } from "./login";
+import { updateSettings } from "./updateSettings";
+
 
 //DOM elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+
 //VALUES
 
 
@@ -22,6 +26,18 @@ if (loginForm) {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         login(email, password);
+    })
+}
+
+if (userDataForm) {
+    userDataForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+
+        updateSettings(name, email);
+        // console.log(name, email);
+
     })
 }
 
